37d27514db44d1d1449529cbb870b4af61bb2367
Stack-Explorer <rajwanivinit560@gmail.com>
2025-10-31 22:31:47 +0530

    Work import removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
-import Work from "./components/Work/Work";
 import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
